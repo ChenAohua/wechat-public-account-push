ad5613b052c84ae3dbc30bb8592d2431899a0abe
ChenAohua <75238187+ChenAohua@users.noreply.github.com>
2022-08-31 22:28:02 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -18,21 +18,21 @@ export const config = {
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如
     // [{
     //   name: "老婆",
-    //   id: "lcl02051111"
+    //   id: "o_7Em6AvJEqPAZ4BpHVSaUQeYksk"
     // },
     // {
     //   name: "李四",
     //   id: "wxafasdasdd-zxfvsdfd"
     // }]
     USERS: [
-      {name: '老婆', id: "cah04081111"}, 
+      {name: '老婆', id: "o_7Em6AvJEqPAZ4BpHVSaUQeYksk"}, 
       {name: '老婆1', id: "2hxOO6VskYv0Kuf-y你懂的,填错狗头打烂RMpuXhk"}, 
       {name: '老婆2', id: "3hxOO6VskYv0Kuf-yw你懂的,填错狗头打烂2RMpuXhk"}, 
       {name: '老婆3', id: "4hxOO6VskYv0Kuf-ywI你懂的,填错狗头打烂MpuXhk"}, 
     ],
     // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
     CALLBACK_USERS: [
-      {name: '自己',id: "cah04081111"}, 
+      {name: '自己',id: "o_7Em6AvJEqPAZ4BpHVSaUQeYksk"}, 
     ],
      
     /**
